e5e8466fd5e3b731aef0b07fc99994e8ec07c73b
Ben Russell <benjaminrussell36@gmail.com>
2020-07-23 09:49:44 -0700

    Add GetAll method to show all categories.
    Co-authored-by: Beverly Potts<pottsbeverly@gmail.com>
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- ToDoList.Tests/ToDoList.Tests/CategoryTests.cs
+++ ToDoList.Tests/ToDoList.Tests/CategoryTests.cs
@@ -42,6 +42,18 @@ namespace ToDoList.Tests
       Assert.AreEqual(1, result);
     }
 
-    [TestMethod
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> {newCategory1, newCategory2};
+
+      List<Category> result = Category.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
