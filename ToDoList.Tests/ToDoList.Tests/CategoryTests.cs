d0cb8a0edae668bbb4e15fdf561153acaa99faa2
Ben Russell <benjaminrussell36@gmail.com>
2020-07-23 09:45:02 -0700

    Add test to check if Ids are assigned to categories. Pass
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-authored-by: Beverly Potts<pottsbeverly@gmail.com>

--- ToDoList.Tests/ToDoList.Tests/CategoryTests.cs
+++ ToDoList.Tests/ToDoList.Tests/CategoryTests.cs
@@ -6,8 +6,12 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class CategoryTest
+  public class CategoryTest : IDisposable
   {
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
 
     [TestMethod]
     public void CategoryConstructor_CreatesInstancesOfCategory_Category()
@@ -15,5 +19,29 @@ namespace ToDoList.Tests
       Category newCategory = new Category("test category");
       Assert.AreEqual(typeof(Category), newCategory.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      string result = newCategory.Name;
+
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      int result = newCategory.Id;
+
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod
   }
 }
